82e1a28c38279b87bbf2c5e4df206919ddbc208f
VinathiG <vinathig18@gmail.com>
2024-04-25 20:06:58 -0400

    code

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Team7_DIS_Assignment3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
